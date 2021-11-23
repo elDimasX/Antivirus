9f436e03c15cb334ca7ec11c7d05a0a0732d4b7f
elDimas <51800283+elDimasX@users.noreply.github.com>
2021-11-23 18:17:05 -0300

    Inicia o Nottext Antivirus
    Exibe uma tela, e inicia o Nottext Antivirus pelo agendador de tarefas (como administrador)

---
+++ Nottext Antivirus Co/Nottext Antivirus Start/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Nottext_Antivirus_Start
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titlo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.guna2CircleProgressBar1 = new Guna.UI2.WinForms.Guna2CircleProgressBar();
+            this.guna2ShadowForm1 = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 12F);
+            this.label2.Location = new System.Drawing.Point(267, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 22);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "2021 © Nottext Antivirus Co.";
+            this.label2.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 12F);
+            this.label1.Location = new System.Drawing.Point(63, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 22);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "O aplicativo está sendo carregado";
+            this.label1.UseWaitCursor = true;
+            // 
+            // titlo
+            // 
+            this.titlo.Font = new System.Drawing.Font("Open Sans", 15.75F);
+            this.titlo.Location = new System.Drawing.Point(63, 25);
+            this.titlo.Name = "titlo";
+            this.titlo.Size = new System.Drawing.Size(337, 44);
+            this.titlo.TabIndex = 13;
+            this.titlo.Text = "Nottext Antivirus - Versão 1.0.3.1";
+            this.titlo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.titlo.UseWaitCursor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 43);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.guna2CircleProgressBar1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.titlo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(492, 275);
+            this.panel1.TabIndex = 37;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // guna2CircleProgressBar1
+            // 
+            this.guna2CircleProgressBar1.Animated = true;
+            this.guna2CircleProgressBar1.AnimationSpeed = 1.2F;
+            this.guna2CircleProgressBar1.FillColor = System.Drawing.Color.White;
+            this.guna2CircleProgressBar1.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal;
+            this.guna2CircleProgressBar1.Location = new System.Drawing.Point(336, 137);
+            this.guna2CircleProgressBar1.Name = "guna2CircleProgressBar1";
+            this.guna2CircleProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(255)))));
+            this.guna2CircleProgressBar1.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(255)))));
+            this.guna2CircleProgressBar1.ProgressThickness = 5;
+            this.guna2CircleProgressBar1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleProgressBar1.ShadowDecoration.Parent = this.guna2CircleProgressBar1;
+            this.guna2CircleProgressBar1.Size = new System.Drawing.Size(40, 40);
+            this.guna2CircleProgressBar1.TabIndex = 37;
+            this.guna2CircleProgressBar1.UseWaitCursor = true;
+            this.guna2CircleProgressBar1.Value = 75;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(496, 279);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(255)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(496, 279);
+            this.MinimumSize = new System.Drawing.Size(496, 279);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nottext Antivirus Start";
+            this.UseWaitCursor = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label titlo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2CircleProgressBar guna2CircleProgressBar1;
+        private Guna.UI2.WinForms.Guna2ShadowForm guna2ShadowForm1;
+    }
+}
+
---
+++ Nottext Antivirus Co/Nottext Antivirus Start/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nottext_Antivirus_Start
+{
+    public partial class Form1 : Form
+    {
+        private void IniciarProcesso(string nome, string argumento)
+        {
+            try
+            {
+                ProcessStartInfo processo = new ProcessStartInfo();
+                processo.FileName = nome;
+                processo.Arguments = argumento;
+                processo.WindowStyle = ProcessWindowStyle.Hidden;
+                Process.Start(processo);
+            }
+            catch (Exception) { }
+        }
+
+        // Configurações para mover uma FORM para o foco
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, ShowWindowEnum flags);
+
+        [DllImport("user32.dll")]
+        private static extern int SetForegroundWindow(IntPtr hwnd);
+
+        // Configurações iniciais
+        private enum ShowWindowEnum
+        {
+            Hide = 0,
+            ShowNormal = 1, ShowMinimized = 2, ShowMaximized = 3,
+            Maximize = 3, ShowNormalNoActivate = 4, Show = 5,
+            Minimize = 6, ShowMinNoActivate = 7, ShowNoActivate = 8,
+            Restore = 9, ShowDefault = 10, ForceMinimized = 11
+        };
+
+        /// <summary>
+        /// Mostrar processo
+        /// </summary>
+        private void BringMainWindowToFront(string nomeProcesso)
+        {
+            try
+            {
+                // Pegar o processo
+                Process processo = Process.GetProcessesByName(nomeProcesso).FirstOrDefault();
+
+                // Checke se o processo está aberto
+                if (processo != null)
+                {
+                    // Check se está minimizado / maximizado
+                    if (processo.MainWindowHandle == IntPtr.Zero)
+                    {
+                        // Mostre a FORM
+                        ShowWindow(processo.Handle, ShowWindowEnum.Restore);
+                    }
+
+                    // Foco na FORM
+                    SetForegroundWindow(processo.MainWindowHandle);
+                }
+                else
+                {
+                    // Se não estiver executando, execute
+                    Process.Start(nomeProcesso);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        /// <summary>
+        /// Operações de estilo
+        /// </summary>
+        public class Styles
+        {
+            /// <summary>
+            /// Quando a FORM for fechada
+            /// </summary>
+            public static async Task Fechar(Form form)
+            {
+                // Opacidade
+                form.Opacity = 1;
+
+                // Repetição
+                while (true)
+                {
+                    // Se for menor ou igual á 0
+                    if (form.Opacity <= 0)
+                    {
+                        break;
+                    }
+
+                    // Aguarde
+                    await Task.Delay(10);
+                    form.Opacity -= .1;
+                }
+            }
+
+            /// <summary>
+            /// Quando a FORM for aberta
+            /// </summary>
+            public static async Task Iniciar(Form form)
+            {
+                // Opacidade
+                form.Opacity = 0;
+
+                // Repetição infinita
+                while (true)
+                {
+                    // Se for maior ou igual á 1
+                    if (form.Opacity >= 1)
+                    {
+                        break;
+                    }
+
+                    // Espere
+                    await Task.Delay(10);
+                    form.Opacity += .1;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Inicie o aplicativo
+        /// </summary>
+        private async void Iniciar()
+        {
+            try
+            {
+                // Se o processo do antivírus já estiver em execução
+                if (Process.GetProcessesByName("NottextAnt").Length > 0)
+                {
+                    BringMainWindowToFront("NottextAnt");
+                    Environment.Exit(0);
+                }
+
+                await Task.Delay(10000);
+
+                // Inicia o processo
+                IniciarProcesso("schtasks.exe", "/run /tn Nottext");
+                await Styles.Fechar(this);
+                Hide();
+
+                // Espere 5 segundos
+                await Task.Delay(5000);
+
+                // Se o processo não tiver sido iniciado
+                if (Process.GetProcessesByName("NottextAnt").Length == 0)
+                {
+                    // Inicie-o
+                    Process.Start(Application.StartupPath + "\\NottextAnt.exe");
+                }
+
+                Environment.Exit(0);
+            } catch (Exception) { }
+        }
+
+        /// <summary>
+        /// Inicia tudo
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            ShadowForm.ApplyShadows(this);
+
+            Iniciar();
+        }
+
+        /// <summary>
+        /// Quando a FORM estiver sendo fechada
+        /// </summary>
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        /// <summary>
+        /// Quando carregado
+        /// </summary>
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await Styles.Iniciar(this);
+        }
+    }
+}
